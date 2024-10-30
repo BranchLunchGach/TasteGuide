c285c11c3bffc9b9a62829252962e9b9aea380cd
BaekDoHyeon <qorehgus0823@naver.com>
2024-10-30 12:42:59 +0900

    BAEK main branch update

---
+++ src/main/java/com/web/spring/repository/MenuRepository.java
@@ -0,0 +1,9 @@
+package com.web.spring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuRepository extends JpaRepository {
+
+}
