f663ec9d92dc83636641f9c68e09692c4e72143a
sunmin_hwang <tommy08190@naver.com>
2024-10-31 18:44:56 +0900

    feat: 메뉴 기능 추가

--- src/main/java/com/web/spring/repository/MenuRepository.java
+++ src/main/java/com/web/spring/repository/MenuRepository.java
@@ -1,11 +1,14 @@
 package com.web.spring.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.web.spring.entity.Menu;
 
-@Repository
 public interface MenuRepository extends JpaRepository<Menu, Integer> {
-
+	Optional<List<Menu>> findByNationInAndMajorCategorieIn(List<String> nation, List<String> majorCategorie);
+//	@Query(value = "select m from Menu m where m.major_categorie in :major_categorie and m.nation in :nation")
+//	List<Menu> getMenuByNationAndMajorCategorie(@Param("major_categorie") List<String> categories, @Param("nation") List<String> nation);
 }
