316b68ffad612453f2d603abd2e3ffc7f9fb40fd
fly9702 <fiy9702@naver.com>
2024-11-04 09:45:24 +0900

    fix jwt

---
+++ src/main/java/com/web/spring/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.web.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@EnableWebSecurity
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class SecurityConfig {
+	
+}
