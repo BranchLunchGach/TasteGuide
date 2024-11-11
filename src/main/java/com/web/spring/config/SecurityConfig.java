13f1ee7a87ca82e0cd0b7c5c08cf16ceaebfd2e4
pleasebelieveme <cain1227@naver.com>
2024-11-11 11:55:02 +0900

    dev -> believeme

--- src/main/java/com/web/spring/config/SecurityConfig.java
+++ src/main/java/com/web/spring/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
                             @Override
                             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                                 CorsConfiguration configuration = new CorsConfiguration();
-                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                                configuration.setAllowedOrigins(Collections.singletonList("http://52.78.178.50:3000"));
                                 configuration.setAllowedMethods(Collections.singletonList("*"));
                                 configuration.setAllowCredentials(true);
 
