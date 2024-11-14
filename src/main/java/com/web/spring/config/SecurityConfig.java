c5973a8a7cbe4694d718baa130f040b8c728f76f
pleasebelieveme <cain1227@naver.com>
2024-11-15 01:20:30 +0900

    fix: OAuth2 Client 디펜던시 추가

--- src/main/java/com/web/spring/config/SecurityConfig.java
+++ src/main/java/com/web/spring/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.web.spring.config;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
@@ -59,7 +60,7 @@ public class SecurityConfig {
                             @Override
                             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                                 CorsConfiguration configuration = new CorsConfiguration();
-                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                                configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://tasteguide.duckdns.org"));
                                 configuration.setAllowedMethods(Collections.singletonList("*"));
                                 configuration.setAllowCredentials(true);
 
