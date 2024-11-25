0c9a67db89afe128540e17de9dfb902b451b3b63
BaekDoHyeon <qorehgus0823@naver.com>
2024-11-25 11:27:05 +0900

    refactor: 레스토랑 크롤링 진행 시 거리 가중치 주석 제거

--- src/main/java/com/web/spring/service/RestaurantService.java
+++ src/main/java/com/web/spring/service/RestaurantService.java
@@ -15,7 +15,7 @@ import com.web.spring.entity.Restaurant;
 import io.jsonwebtoken.lang.Arrays;
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
+@Slf4j 	
 @Service
 public class RestaurantService {
 	
@@ -175,28 +175,28 @@ public class RestaurantService {
 			log.info("[RestaurantService] restaurantRecommend() : {} 메인 키워드 가중치 끝 & 거리 가중치 시작...", datas[0]);
 			
 //			거리에 따른 가중치 부여
-//			String address = mapAPI.getGeocode(datas[3]);
-//			String endX = address.split(",")[0];
-//			String endY = address.split(",")[1];
-//			
-//			System.out.println("startX >> " + startX);
-//			System.out.println("startY >> " + startY);
-//			System.out.println("endX >> " + endX);
-//			System.out.println("endY >> " + endY);
-//			String strDistance = mapAPI.getLinearDistance(startX, startY, endX, endY);
-//			int distance = Integer.parseInt(strDistance);
-//			
-//			if (distance <= 300) {
-//				score += 20;
-//			} else if (distance <= 600) {
-//				score += 10;
-//			} else if (distance <= 1000) {
-//				score += 5;
-//			} else if (distance <= 2000) {
-//				score += 1;
-//			} else if (distance > 2000) {
-//				score -= 20;
-//			}
+			String address = mapAPI.getGeocode(datas[3]);
+			String endX = address.split(",")[0];
+			String endY = address.split(",")[1];
+			
+			System.out.println("startX >> " + startX);
+			System.out.println("startY >> " + startY);
+			System.out.println("endX >> " + endX);
+			System.out.println("endY >> " + endY);
+			String strDistance = mapAPI.getLinearDistance(startX, startY, endX, endY);
+			int distance = Integer.parseInt(strDistance);
+			
+			if (distance <= 300) {
+				score += 20;
+			} else if (distance <= 600) {
+				score += 10;
+			} else if (distance <= 1000) {
+				score += 5;
+			} else if (distance <= 2000) {
+				score += 1;
+			} else if (distance > 2000) {
+				score -= 20;
+			}
 			
 			//가중치 로직 끝...
 			log.info("[RestaurantService] restaurantRecommend() : {} 데이터 가공 끝 & Restaurant 객체 생성 후 Queue.add()", datas[0]);
